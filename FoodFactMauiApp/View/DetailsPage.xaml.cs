5e88d3b9d1eec88041594c3527f72fe334ed6df8
pascallenoir <angepatriceadou@gmail.com>
2024-03-28 12:18:44 +0000

    Creation de details page et viewmodel Rajout du bouton "voir sur open food fact"

--- FoodFactMauiApp/View/DetailsPage.xaml.cs
+++ FoodFactMauiApp/View/DetailsPage.xaml.cs
@@ -2,11 +2,9 @@ namespace FoodFactMauiApp.View;
 
 public partial class DetailsPage : ContentPage
 {
-	ProductDetailsViewModel viewModel;
 	public DetailsPage(ProductDetailsViewModel productDetailsViewModel)
 	{
 		InitializeComponent();
-		this.viewModel = productDetailsViewModel;
-		BindingContext = viewModel;
+		BindingContext = productDetailsViewModel;
 	}
 }
--- FoodFactMauiApp/ViewModel/ProductDetailsViewModel.cs
+++ FoodFactMauiApp/ViewModel/ProductDetailsViewModel.cs
@@ -5,4 +5,23 @@ public partial class ProductDetailsViewModel :BaseViewModel
 {
     [ObservableProperty]
     Product product;
+
+
+    [RelayCommand]
+    async Task OpenProductAsync(Product product)
+    {
+        if (product is null) 
+            return;
+
+        try
+        {
+            Uri uri = new(product.Url);
+            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+        }
+        catch (Exception ex)
+        {
+           Debug.WriteLine(ex.Message);
+            await Shell.Current.DisplayAlert("Erreur", "Impossible d'ouvrir le navigateur", "Ok");
+        }
+    }
 }
